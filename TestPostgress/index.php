fe7b151481c7f750c4cc68cf530efddb5069da2b
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-08-13 14:02:48 +0700

    sdfsdfs

--- TestPostgress/index.php
+++ TestPostgress/index.php
@@ -5,8 +5,18 @@
 </head>
 <body>
 	<?php  
-		$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
-		echo "done";
+		// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
+		// echo "done";
+
+		$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		));
 		
 		$sql = "SELECT studentname, course FROM registercourse";
 		$stmt = $pdo->prepare($sql);
